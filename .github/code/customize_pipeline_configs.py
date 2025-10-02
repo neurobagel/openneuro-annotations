96386992bead1f8c2e9a9cc6a0d609a2dea11545
Alyssa Dai <alyssa.ydai@gmail.com>
2025-10-02 17:19:17 -0400

    parametrize output dir for script that customizes pipeline configs

--- .github/code/customize_pipeline_configs.py
+++ .github/code/customize_pipeline_configs.py
@@ -3,55 +3,50 @@ from pathlib import Path
 import argparse
 
 
-EXAMPLE_CONFIG={
-    "NAME": "fmriprep",
-    "VERSION": "",
-    "STEPS": [
-        {
-            "TRACKER_CONFIG_FILE": "tracker.json"
-        }
-    ],
-    "PIPELINE_TYPE": "processing",
-    "SCHEMA_VERSION": 1
-}
-
-EXAMPLE_TRACKER={
-    "PATHS": [
-        "[[NIPOPPY_BIDS_PARTICIPANT_ID]]/[[NIPOPPY_BIDS_SESSION_ID]]/anat/[[NIPOPPY_BIDS_PARTICIPANT_ID]]_[[NIPOPPY_BIDS_SESSION_ID]]*_T1w.json",
-        "[[NIPOPPY_BIDS_PARTICIPANT_ID]]_[[NIPOPPY_BIDS_SESSION_ID]]*_T1w.html"
-    ]
-}
-
-
 def load_json(path: Path) -> dict:
     with open(path, 'r') as f:
         return json.load(f)
 
 
-def remove_session_placeholders(tracker: dict) -> dict:
+def save_json(data: dict, path: Path):
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+def remove_session_placeholders_from_paths(paths: list) -> list:
     new_paths = []
-    for path in tracker["PATHS"]:
+    for path in paths:
         new_path = path.replace("[[NIPOPPY_BIDS_SESSION_ID]]/", "")
         new_path = new_path.replace("_[[NIPOPPY_BIDS_SESSION_ID]]", "")
         new_paths.append(new_path)
-    tracker["PATHS"] = new_paths
-    return tracker
+    return new_paths
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Customize pipeline configurations")
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Customize configuration files for a pipeline version.")
     parser.add_argument("config", type=Path, help="Path to the config.json")
     parser.add_argument("tracker", type=Path, help="Path to the tracker.json")
     parser.add_argument("version", type=str, help="Pipeline version")
+    parser.add_argument("output_dir", type=Path, help="Output directory for the pipeline configuration files")
     parser.add_argument(
         "--no-sessions", action="store_true", help="Indicate that the dataset does not have sessions"
     )
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
 
     config = load_json(args.config)
     tracker = load_json(args.tracker)
 
     config["VERSION"] = args.version
-
     if args.no_sessions:
-        tracker = remove_session_placeholders(tracker)
+        tracker["PATHS"] = remove_session_placeholders_from_paths(tracker["PATHS"])
+
+    save_json(config, args.output_dir / "config.json")
+    save_json(tracker, args.output_dir / "tracker.json")
+
+
+if __name__ == "__main__":
+    main()
