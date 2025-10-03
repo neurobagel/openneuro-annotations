9c51510530a2318b6a6e07b25ae7fc3f6c362f9e
Alyssa Dai <alyssa.ydai@gmail.com>
2025-10-03 12:06:09 -0400

    generate processing_status.tsv files

--- .github/code/create_nipoppy_dataset.sh
+++ .github/code/create_nipoppy_dataset.sh
@@ -11,6 +11,7 @@ SOURCE_WORKDIR="derivatives/source"
 NIPOPPY_WORKDIR="derivatives/nipoppy"
 TEMPLATE_CONFIGS_DIR="derivatives/template_pipeline_configs"
 NIPOPPY_DATASET_DIR="${NIPOPPY_WORKDIR}/${DS_ID}"
+OUTPUT_DIR="processing_status_files/"
 
 mkdir -p ${SOURCE_WORKDIR}
 mkdir -p ${NIPOPPY_WORKDIR}
@@ -38,9 +39,9 @@ while IFS= read -r derivative_ds; do
         derivative_ds_url="https://github.com/OpenNeuroDerivatives/${derivative_ds}.git"
         pipeline_name=$(jq -r '.name | ascii_downcase' <<< "$pipeline_info")
         pipeline_version=$(jq -r '.version' <<< "$pipeline_info")
-        derivative_dir="${NIPOPPY_DATASET_DIR}/derivatives/${pipeline_name}/${pipeline_version}/output"
-        mkdir -p "${derivative_dir}"
-        git clone -q "${derivative_ds_url}" "${derivative_dir}"
+        pipeline_dir="${NIPOPPY_DATASET_DIR}/derivatives/${pipeline_name}/${pipeline_version}/output"
+        mkdir -p "${pipeline_dir}"
+        git clone -q "${derivative_ds_url}" "${pipeline_dir}"
 
         pipeline_config_dir="${NIPOPPY_DATASET_DIR}/pipelines/processing/${pipeline_name}-${pipeline_version}"
         mkdir -p "${pipeline_config_dir}"
@@ -56,10 +57,21 @@ while IFS= read -r derivative_ds; do
         fi
         echo "${DS_ID}: ${derivative_ds}: Customizing config.json and tracker.json files"
         python ${SCRIPT_DIR}/customize_configs_for_pipeline.py "${args[@]}"
+
+        echo "${DS_ID}: ${derivative_ds}: Tracking processing statuses"
+        nipoppy track-processing "${NIPOPPY_DATASET_DIR}" \
+            --pipeline "${pipeline_name}" \
+            --pipeline-version "${pipeline_version}"
     else
         echo "${DS_ID}: ${derivative_ds}: WARNING No pipeline info found, skipping."
     fi
 
 done <<< "$derivative_datasets"
+
+processing_status_file="${NIPOPPY_DATASET_DIR}/derivatives/processing_status.tsv"
+if [ -f "$processing_status_file" ]; then
+    cp "$processing_status_file" "${OUTPUT_DIR}/${DS_ID}.tsv"
+fi
+echo "${DS_ID}: Processing status file copied to ${OUTPUT_DIR}/${DS_ID}.tsv"
 
 echo "${DS_ID}: Done."
